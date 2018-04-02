dd823912ef881bae337a1234fa318901d7924421
wanghongwei2016 <973119204@qq.com>
2018-04-02 14:18:02 +0800

    fix

--- xs-tj/src/main/java/com/xiangshui/tj/server/service/DataManager.java
+++ xs-tj/src/main/java/com/xiangshui/tj/server/service/DataManager.java
@@ -3,7 +3,6 @@ package com.xiangshui.tj.server.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
