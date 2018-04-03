1fffccc876cdca8174f27fc8b671afec9f38765c
wanghongwei2016 <973119204@qq.com>
2018-04-03 09:51:22 +0800

    fix

--- xs-tj/src/main/java/com/xiangshui/tj/server/service/DataManager.java
+++ xs-tj/src/main/java/com/xiangshui/tj/server/service/DataManager.java
@@ -3,13 +3,14 @@ package com.xiangshui.tj.server.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 abstract public class DataManager<K, V> {
     private static final Logger log = LoggerFactory.getLogger(DataManager.class);
 
-    private Map<K, V> map = new ConcurrentHashMap();
+    private Map<K, V> map = new Hashtable();
 
     public V getById(K id) {
         return map.get(id);
--- xs-tj/src/main/java/com/xiangshui/tj/web/result/Result.java
+++ xs-tj/src/main/java/com/xiangshui/tj/web/result/Result.java
@@ -1,32 +1,39 @@
 package com.xiangshui.tj.web.result;
 
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Result {
 
-    private int code;
-    private String msg;
-    private Object data;
+    public int code;
+    public String msg;
+    public Map<String, Object> data;
 
-    public Result(int code, String msg, Object data) {
+    public Result(int code, String msg) {
         this.code = code;
         this.msg = msg;
-        this.data = data;
     }
 
-    public int getCode() {
-        return code;
+    public Result(CodeMsg codeMsg) {
+        this.code = codeMsg.code;
+        this.msg = codeMsg.msg;
     }
 
-    public String getMsg() {
-        return msg;
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
     }
 
-    public Object getData() {
-        return data;
-    }
+    public Result putData(String key, Object value) {
 
-
-    public static void main(String[] args) {
+        if (data == null) {
+            data = new HashMap<String, Object>();
+        }
+        data.put(key, value);
+        return this;
     }
 
 
