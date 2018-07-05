edbe164aea4587e5b765681e58894b04c2696fae
wanghongwei2016 <973119204@qq.com>
2018-07-05 18:04:42 +0800

    fix

--- xs-server/src/main/java/com/xiangshui/server/Test.java
+++ xs-server/src/main/java/com/xiangshui/server/Test.java
@@ -3,21 +3,28 @@ package com.xiangshui.server;
 import com.alibaba.fastjson.JSON;
 import com.amazonaws.services.dynamodbv2.document.AttributeUpdate;
 import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
+import com.amazonaws.services.dynamodbv2.document.ScanFilter;
 import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
-import com.xiangshui.server.dao.AreaDao;
-import com.xiangshui.server.dao.CapsuleDao;
-import com.xiangshui.server.dao.CityDao;
-import com.xiangshui.server.dao.UserInfoDao;
+import com.xiangshui.server.dao.*;
 import com.xiangshui.server.domain.Area;
+import com.xiangshui.server.domain.AreaContract;
 import com.xiangshui.server.domain.Capsule;
 import com.xiangshui.server.domain.UserInfo;
+import com.xiangshui.server.domain.mysql.Op;
+import com.xiangshui.server.example.OpExample;
+import com.xiangshui.server.mapper.OpMapper;
 import com.xiangshui.server.service.PartnerService;
+import com.xiangshui.util.ExcelUtils;
+import com.xiangshui.util.spring.SpringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.FileInputStream;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -40,6 +47,12 @@ public class Test {
     @Autowired
     CityDao cityDao;
 
+    @Autowired
+    OpMapper opMapper;
+
+    @Autowired
+    AreaContractDao areaContractDao;
+
 
     public void testSelect() throws Exception {
         List<UserInfo> userInfoList = userInfoDao.scan(new ScanSpec().withFilterExpression("phone = :phone").withValueMap(new ValueMap().withString(":phone", "11000000014")));
@@ -61,21 +74,67 @@ public class Test {
 
     public void test() throws Exception {
 
-        List<Capsule> capsuleList = capsuleDao.batchGetItem("capsule_id", new Object[]{1100002005});
+        Date now=new Date();
 
-        log.debug(JSON.toJSONString(capsuleList));
+        List<List<String>> data = ExcelUtils.read(new FileInputStream("/Users/whw/Downloads/分账信息.xlsx"), 0);
+        for (List<String> row : data) {
+            try {
+                String area_title = row.get(0);
+                String saler = row.get(3);
+                String customer = row.get(4);
+                String account_ratio_str = row.get(5);
+                String bank_account = row.get(6);
+                String bank_branch = row.get(7);
+
+                if (StringUtils.isBlank(area_title)) {
+                    continue;
+                }
+                if (StringUtils.isBlank(saler)) {
+                    continue;
+                }
+
+                OpExample example = new OpExample();
+                example.setLimit(1);
+                example.createCriteria().andFullnameEqualTo(saler);
+                List<Op> opList = opMapper.selectByExample(example);
+                if (opList != null && opList.size() > 0) {
+                    Op op = opList.get(0);
+
+                    List<Area> areaList = areaDao.scan(new ScanSpec().withScanFilters(new ScanFilter("title").eq(area_title)));
+
+                    if (areaList != null && areaList.size() > 0) {
+                        Area area = areaList.get(0);
+                        int account_ratio = (int) (Float.valueOf(account_ratio_str) * 100);
+                        AreaContract areaContract = new AreaContract();
+                        areaContract.setArea_id(area.getArea_id());
+                        areaContract.setSaler(op.getFullname());
+                        areaContract.setSaler_city(op.getCity());
+                        areaContract.setCustomer(customer);
+                        areaContract.setBank_account_name(customer);
+                        areaContract.setBank_account(bank_account);
+                        areaContract.setBank_branch(bank_branch);
+                        areaContract.setAccount_ratio(account_ratio);
+                        areaContract.setStatus(0);
+                        areaContract.setCreate_time(now.getTime()/1000);
+                        areaContract.setUpdate_time(now.getTime()/1000);
+                        areaContractDao.putItem(areaContract);
+                        continue;
+                    }
+                }
+                System.out.println(area_title);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
     public static void main(String[] args) throws Exception {
 
 
-//        SpringUtils.init();
-//        SpringUtils.getBean(Test.class).test();
+        SpringUtils.init();
+        SpringUtils.getBean(Test.class).test();
 
-        Set<String> set=JSON.parseObject("['123','sdf','123']", Set.class);
-
-        System.out.println(JSON.parseObject("['123']", Set.class));
 
     }
 }
--- xs-utils/src/main/java/com/xiangshui/util/ExcelUtils.java
+++ xs-utils/src/main/java/com/xiangshui/util/ExcelUtils.java
@@ -1,10 +1,18 @@
 package com.xiangshui.util;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ExcelUtils {
@@ -24,5 +32,70 @@ public class ExcelUtils {
         return workbook;
     }
 
+    public static List<List<String>> read(InputStream inputStream, int sheetIndex) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+        if (workbook == null) {
+            System.out.println("未读取到内容,请检查路径！");
+            return null;
+        }
+        XSSFSheet sheet = workbook.getSheetAt(sheetIndex);
+        return read(sheet);
+    }
+
+    public static List<List<String>> read(InputStream inputStream, String sheetName) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+        if (workbook == null) {
+            System.out.println("未读取到内容,请检查路径！");
+            return null;
+        }
+        XSSFSheet sheet = workbook.getSheet(sheetName);
+        return read(sheet);
+    }
+
+    public static List<List<String>> read(XSSFSheet sheet) throws IOException {
+        if (sheet == null) return null;
+        List<List<String>> data = new ArrayList<List<String>>();
+        if (sheet != null) {
+            for (Iterator<Row> rowIterator = sheet.iterator(); rowIterator.hasNext(); ) {
+                Row row = rowIterator.next();
+                List<String> rowDate = new ArrayList<String>();
+                for (Iterator<Cell> cellIterator = row.cellIterator(); cellIterator.hasNext(); ) {
+                    Cell cell = cellIterator.next();
+                    rowDate.add(getValue((XSSFCell) cell));
+                }
+                data.add(rowDate);
+            }
+        }
+        return data;
+    }
+
+    private static String getValue(XSSFCell cell) {
+        if (cell == null) {
+            return "---";
+        }
+        if (cell.getCellType() == cell.CELL_TYPE_BOOLEAN) {
+            return String.valueOf(cell.getBooleanCellValue());
+        } else if (cell.getCellType() == cell.CELL_TYPE_NUMERIC) {
+            double cur = cell.getNumericCellValue();
+            long longVal = Math.round(cur);
+            Object inputValue = null;
+            if (Double.parseDouble(longVal + ".0") == cur)
+                inputValue = longVal;
+            else
+                inputValue = cur;
+            return String.valueOf(inputValue);
+        } else if (cell.getCellType() == cell.CELL_TYPE_BLANK || cell.getCellType() == cell.CELL_TYPE_ERROR) {
+            return "---";
+        } else {
+            return String.valueOf(cell.getStringCellValue());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        List<List<String>> data = read(new FileInputStream("/Users/whw/Downloads/分账信息.xlsx"), 0);
+
+        System.out.println();
+    }
 
 }
