7d29b3d34d443870bd63aca2474a7f4150227b5f
wanghongwei2016 <973119204@qq.com>
2019-04-10 10:55:29 +0800

    u

--- xs-op/src/main/webapp/static/js/mnt_bill_manage.js
+++ xs-op/src/main/webapp/static/js/mnt_bill_manage.js
@@ -83,7 +83,7 @@ class Page extends React.Component {
             <MinitouBillGrid renderHeader={(headerHtml, data, columns) => {
                 return [
                     headerHtml,
-                    <span className="text-danger pl-3">本月净利润{(columns[3].total - columns[4].total - 22000).toFixed(2)}元（扣除固定成本2.2万元）</span>
+                    <span className="text-danger pl-3">本月净利润{(columns[2].total - columns[3].total - 22000).toFixed(2)}元（扣除固定成本2.2万元）</span>
                 ];
             }} ref="grid"></MinitouBillGrid>
             <ModalContainer></ModalContainer>
