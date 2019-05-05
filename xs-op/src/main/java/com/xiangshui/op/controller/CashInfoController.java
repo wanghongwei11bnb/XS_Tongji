60394b7f8d4368626517f9b50c98d7ea5901a3fa
wanghongwei2016 <973119204@qq.com>
2019-05-05 17:36:13 +0800

    1

--- xs-op/src/main/java/com/xiangshui/op/controller/CashInfoController.java
+++ xs-op/src/main/java/com/xiangshui/op/controller/CashInfoController.java
@@ -105,6 +105,8 @@ public class CashInfoController extends BaseController {
                                     return "发放";
                                 case 2:
                                     return "提现";
+                                case 3:
+                                    return "充值";
                                 default:
                                     return null;
                             }
